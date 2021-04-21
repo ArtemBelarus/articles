ec970b4c8614b2d11e0bf1df639f9254fb059fea
ArtemZ <a.zhorov@papakaya.com>
2021-04-21 19:29:31 +0300

    simple test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Services\ArticleService;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Mockery;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -11,8 +14,17 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testArticlesController()
     {
-        $this->assertTrue(true);
+        $service = Mockery::mock(ArticleService::class);
+        app()->instance(\App\Services\ArticleService::class, $service);
+
+        $service
+            ->shouldReceive('getAllArticles')
+            ->once()
+            ->andReturn(new LengthAwarePaginator([], 0, 1));
+
+        $response = $this->get(route('articles.index'));
+        $response->assertStatus(200);
     }
 }
